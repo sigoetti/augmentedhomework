9c3759b780fc4e1babd78799afce4c6f52847dbe
Silvia Götti <dev@silviagoetti.ch>
2020-03-30 14:32:37 +0200

    readme and examples

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
-# augmentedhomework
+#Augmented Homework
 
-Spice up your homework with 3d learning
+I've read in the newspapers that Swiss teachers had to switch to homeschooling over one weekend. Because there was nothing digital in place they sent out big packages of paper assignments. But I think, sitting a home having to work through a big package of paper assignments must be boring.
 
-A project for #CodeVsCOVID19
+##Idea
+My idea is to use the existing homework sheets, but to augment the pictures with videos or 3d models. Like in Harry Potter. Many children do have a recent smartphone that is capable of displaying Augmented Reality. This could be cool and motivating. I've seen videos of augmented advertisements online so that was I wanted to do for schools.
+
+##Approach
+I've augmented paper before, using web technologies. It works, but not really good and is often dependent on the browser version. I recently began to learn Flutter and did a small AR project. So I wanted to do a Flutter AR app.
+
+##Technologies used
+* [Flutter](https://flutter.dev/)
+* [ARCore Package](https://github.com/giandifra/arcore_flutter_plugin)
+
+##Learnings
+I've learnt that this is not as easy as I thought. The Flutter AR package is still too experimental to do videos and so I did experiments with classic Java code to learn what's under the hood. I found a tutorial but I'm still trying to understand. 
+
+##State of the project
+I'm currently stuck in learning the tech. I can display one 3d model, but I actually want to do videos. There's still a lot of work to do. 
+
+##Future
+Add a set of matched homework/content to the app itself and then build a platform where teachers can upload the AR content for the app. 
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:video_player/video_player.dart';
 
-void main() => runApp( MyHomePage());
+void main() => runApp( AugmentedPage());
 
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
+class AugmentedPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _AugmentedPageState createState() => _AugmentedPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _AugmentedPageState extends State<AugmentedPage> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
   ArCoreController arCoreController;
   Map<int, ArCoreAugmentedImage> augmentedImagesMap = Map();
 
   @override
+  void initState() {
+    // Create an store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    super.initState();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AugmentedPage'),
+        ),
+        body: ArCoreView(
+          onArCoreViewCreated: _onArCoreViewCreated,
+          type: ArCoreViewType.AUGMENTEDIMAGES,
+        ),
       ),
-      body: ArCoreView(
-        onArCoreViewCreated: _onArCoreViewCreated,
-        type: ArCoreViewType.AUGMENTEDIMAGES,
-      ),
-
     );
   }
 
-  void _onArCoreViewCreated(ArCoreController controller) {
+  void _onArCoreViewCreated(ArCoreController controller) async {
     arCoreController = controller;
     arCoreController.onTrackingImage = _handleOnTrackingImage;
     loadSingleImage();
-
+    //OR
+    //loadImagesDatabase();
   }
 
   loadSingleImage() async {
@@ -45,19 +59,63 @@ class _MyHomePageState extends State<MyHomePage> {
     await rootBundle.load('assets/earth_augmented_image.jpg');
     arCoreController.loadSingleAugmentedImage(
         bytes: bytes.buffer.asUint8List());
-
   }
+
 
 
   _handleOnTrackingImage(ArCoreAugmentedImage augmentedImage) {
     if (!augmentedImagesMap.containsKey(augmentedImage.index)) {
       augmentedImagesMap[augmentedImage.index] = augmentedImage;
+      _addSphere(augmentedImage);
     }
+  }
+
+  Future _addSphere(ArCoreAugmentedImage augmentedImage) async {
+  /*  final ByteData textureBytes = await rootBundle.load('assets/earth.jpg');
+
+    final material = ArCoreMaterial(
+      color: Color.fromARGB(120, 66, 134, 244),
+      textureBytes: textureBytes.buffer.asUint8List(),
+    );
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: augmentedImage.extentX / 2,
+    );
+    final node = ArCoreNode(
+      shape: sphere,
+    );*/
+
+    // Use a FutureBuilder to display a loading spinner while waiting for the
+// VideoPlayerController to finish initializing.
+    FutureBuilder(
+      future: _initializeVideoPlayerFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          // If the VideoPlayerController has finished initialization, use
+          // the data it provides to limit the aspect ratio of the VideoPlayer.
+          return AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            // Use the VideoPlayer widget to display the video.
+            child: VideoPlayer(_controller),
+          );
+        } else {
+          // If the VideoPlayerController is still initializing, show a
+          // loading spinner.
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+
+
+
+    arCoreController.addArCoreNodeToAugmentedImage(node, augmentedImage.index);
   }
 
   @override
   void dispose() {
     arCoreController.dispose();
+    _controller.dispose();
+
     super.dispose();
   }
 }
